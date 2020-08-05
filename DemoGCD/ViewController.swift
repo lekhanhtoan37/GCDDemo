2e3de7504d8c1482e0ab6a7e1bf51e641271823c
toanlk <toanlk@solarapp.asia>
2020-08-05 08:04:16 +0700

    Initial Commit

---
+++ DemoGCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGCD
+//
+//  Created by Le Toan on 8/5/20.
+//  Copyright © 2020 Le Toan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
